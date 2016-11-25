f9aaeac21f1319c90680a19012a416dca94330b5
han zhang <316931656@qq.com>
2016-11-25 16:31:45 +0800

    数据库里面取不出东西  老子好气啊

---
+++ src/main/java/com/zhang/wsp/controller/RuleController.java
@@ -0,0 +1,26 @@
+package com.zhang.wsp.controller;
+
+import com.zhang.wsp.dao.RuleDao;
+import com.zhang.wsp.model.Rule;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Created by zhang on 2016/11/25.
+ */
+@Controller
+@RequestMapping("/rule")
+public class RuleController {
+    public RuleDao ruleDao;
+    @RequestMapping("/show")
+    public String showRule(HttpServletRequest request, Model model){
+        List<Rule> list= ruleDao.selectAllRule();
+        return "success";
+    }
+
+
+}
